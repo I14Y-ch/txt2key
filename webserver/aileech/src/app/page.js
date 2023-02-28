12074af040ed6ad29b3e609d5f71ae548398ca82
Giuseppe Capnetto <ggcaponetto@gmail.com>
2023-02-28 22:10:09 +0100

    wip

--- webserver/aileech/src/app/page.js
+++ webserver/aileech/src/app/page.js
@@ -5,17 +5,20 @@ import styles from './page.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 async function getData() {
-  return new Promise((res) => {
+  /*return new Promise((res) => {
     setTimeout(() => {
       res("Dynamic fetched data on the server");
     }, 2000)
-  });
+  });*/
+
+  return fetch('https://jsonplaceholder.typicode.com/comments/'+Math.floor(Math.random() * 10), { cache: 'no-store' })
+      .then(response => response.json())
 }
 export default async function Home() {
   const data = await getData();
   return (
     <main className={styles.main}>
-      <h1>{data}</h1>
+      <h1>{JSON.stringify(data)}</h1>
     </main>
   )
 }
