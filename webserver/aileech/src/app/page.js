b743679eb913db2ce54519e057deefc05c596255
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2023-03-01 13:29:24 +0100

    wip

--- webserver/aileech/src/app/page.js
+++ webserver/aileech/src/app/page.js
@@ -10,15 +10,23 @@ async function getData() {
       res("Dynamic fetched data on the server");
     }, 2000)
   });*/
-
-  return fetch('https://jsonplaceholder.typicode.com/comments/'+Math.floor(Math.random() * 10), { cache: 'no-store' })
+  return fetch('http://localhost:3000/live-leech', {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      "Content-Type": "application/json"
+    },
+    cache: 'no-store',
+    body: JSON.stringify({
+      prompt: `Is ${Date.now()} grater than ${Date.now()-1000}?`
+    }), // body data type must match "Content-Type" header
+  })
       .then(response => response.json())
 }
 export default async function Home() {
   const data = await getData();
   return (
     <main className={styles.main}>
-      <h1>{JSON.stringify(data)}</h1>
+      <h1>{data.choices[0].text}</h1>
     </main>
   )
 }
