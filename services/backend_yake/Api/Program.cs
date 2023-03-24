4307112ee777f1cbd3e4062c949dd0ac9f3e8734
Schulungsuser <Schulungsuser@L1168.int.rubicon-it.com>
2023-03-24 13:15:28 +0100

    added generated termdat client

--- services/backend_yake/Api/Program.cs
+++ services/backend_yake/Api/Program.cs
@@ -1,3 +1,7 @@
+using Api.TermDatClient;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Linq;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -34,11 +38,43 @@ app.MapPost("/keywords", async (HttpContext httpContext) =>
     }
 
     var yakeResult = await yakeResponse.Content.ReadFromJsonAsync<YakeResult[]>();
+    var filteredYakeResult = yakeResult.Where(x => x.score <= 0.15).ToList();
 
-    return Results.Json(yakeResult!
-        .Where(result => result.score < 0.15)
-        .Select(result => new Txt2KeyResult(result.ngram, postBody.language))
-        .ToArray());
+    var termDatClient = new Client("https://api.termdat.bk.admin.ch", httpClient);
+
+    var result = filteredYakeResult!.Select(x => new Txt2KeyResult(x.ngram, languageForYake)).ToList();
+
+    var taskList = filteredYakeResult.Select(x => termDatClient.VSearchAsync(new[] { 1110, 102, 11446, 10426 }, new[] { 1, 2, 24, 3, 4, 22, 5, 6, 7, 8, 23, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 }, null, null, null, null, null, null, null, null, null, null, null, languageForYake, null, languageForYake, x.ngram, ReturnType.Summary, "2"));
+    await Task.WhenAll(taskList.ToArray());
+
+
+    foreach (var item in filteredYakeResult)
+    {
+        var termDatSearchResponse = await termDatClient.VSearchAsync(new[] { 1110, 102, 11446, 10426 }, new[] { 1, 2, 24, 3, 4, 22, 5, 6, 7, 8, 23, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 }, null, null, null, null, null, null, null, null, null, null, null, languageForYake, null, languageForYake, item.ngram, ReturnType.Summary, "2");
+        if(termDatSearchResponse.Result.Count <= 0)
+        {
+            continue;
+        }
+        var termDatEntryId = termDatSearchResponse.Result.First().Id;
+
+        var termDatEntryResponseDeEn = await termDatClient.VEntryAsync("de", "en", new[] { termDatEntryId }, "2");
+        var termDatEntryResponsFrIt = await termDatClient.VEntryAsync("fr", "it", new[] { termDatEntryId }, "2");
+        var languageDetailsArray = termDatEntryResponseDeEn.Result.FirstOrDefault().LanguageDetails.ToArray();
+        languageDetailsArray.Concat(termDatEntryResponsFrIt.Result.FirstOrDefault().LanguageDetails.ToArray());
+
+        foreach (var keyword in languageDetailsArray)
+        {
+            if (keyword.Name != null && keyword.Name.ToLower().Contains(item.ngram.ToLower()))
+            {
+                var index = Array.IndexOf(languageDetailsArray, keyword);
+                result.Add(new Txt2KeyResult(languageDetailsArray[index].Name, languageDetailsArray[index].LanguageIsoCode));
+                result.Add(new Txt2KeyResult(languageDetailsArray[index * 2 + 1].Name, languageDetailsArray[index * 2 + 1].LanguageIsoCode));
+            }
+        }
+    }
+
+
+    return Results.Json(result);
 }).WithOpenApi().Accepts<Txt2KeyRequest>("application/json");
 
 app.Run();
@@ -71,3 +107,30 @@ public record Txt2KeyRequest
     string publisher,
     string language = "de"
 );
+
+public record TermDatSearchRequest
+(
+    int[] collectionIds,
+    string inLanguageCode,
+    string searchTerm,
+    string returnType = "Summary"
+);
+
+public record TermDatSearchResponse
+(
+      int id,
+      string url,
+      TermDatStatus status,
+      TermDatReliability reliability,
+      TermDatOffice office,
+      TermDatCollection collection,
+      TermDatClassification classification,
+      TermDatHits[] hits
+);
+
+public record TermDatStatus(string code, string text);
+public record TermDatReliability(string code, string text);
+public record TermDatOffice(int id, string code,  string text);
+public record TermDatCollection(int it, string code, string text);
+public record TermDatClassification(int it, string code, string text);
+public record TermDatHits(string name0);
