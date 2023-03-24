7ecf089f5303bb7378277a7afa4a31142945ec34
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2023-03-24 11:14:11 +0100

    wip

--- gui-react/public/js/txt2key.js
+++ gui-react/public/js/txt2key.js
@@ -1,22 +1,88 @@
 
-
+const api = {
+    queryChatGPT: async function queryChatGPT(form = {}){
+        return fetch("https://govtech-service.azurewebsites.net/keywords", {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify({
+                title: 'Swiss Standard Classification of Occupations CH-ISCO-19 v.1.2',
+                description: 'The Swiss Standard Classification of Occupations CH-ISCO-19 continues to use the first four levels of the International Standard Classification of Occupations ISCO-08 and includes an additional fifth level to take account of the particularities of the labour market in Switzerland. It also contains the occupation titles attributed to each category. These occupational titles group together the occupations most often mentioned in FSO surveys, those consulted by professional and managerial associations and the official titles listed by the State Secretariat for Education, Research and Innovation (SERI). This list is updated regularly.\n\nThis new classification replaces the Swiss Standard Classification of Occupations from 2000 (SSCO2000).\n\nRoutine revisions: Possible adaptations of the 5th level (types of occupation) every 3 years (1st time Octobre 2022) to meet national needs.\nMethodology revisions: According to the periodic revisions of the International Standard Classification of Occupations ISCO (ISCO)',
+                publisher: 'Federal Statistical Office',
+                topics: ['Official statistics', 'Labour'],
+                language: "de",
+                ...form
+            }), // body data type must match "Content-Type" header
+        });
+    },
+    queryRAKE: async function queryRAKE(form = {}){
+        return fetch("https://api20230324101937.azurewebsites.net/keywords", {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify({
+                "title": "Some cool title",
+                "description": "Some cool description",
+                "topics": [
+                    "Buildings", "Energy"
+                ],
+                "publisher": "Mario Rossi",
+                language: "de",
+                ...form,
+            }), // body data type must match "Content-Type" header
+        });
+    }
+}
 function run(){
     function dispatchTxt2KeyEvent(config, data){
         /* Broadcast the event to the host so that the UI can be updated */
         window.dispatchEvent(
             new CustomEvent("txt2key", { detail: {
-                    config, data
+                    config, data, type: "response"
                 }})
         );
 
         const nodeId = "txt2key"
         const txt2keyNode = document.getElementById(nodeId);
         const css = `position: absolute; top: ${config.css.top}; left: ${config.css.left}; bottom: ${config.css.bottom}; right: ${config.css.right}; margin: 20px; background: rgba(255, 255, 255, 1); color: black; padding: 15px; opacity: 0.5`
+
+        let gpt;
+        let rake;
+        try {
+            gpt = data.filter(d => d.type === "gpt")[0].data.keywords;
+        } catch (e){
+            console.error("error", e);
+        }
+        try {
+            rake = data.filter(d => d.type === "rake")[0].data;
+        } catch (e){
+            console.error("error", e);
+        }
+
         const content = `
                     <div class="txt2key-content" style="${css}">
-                        <h4>TXT2KEY Co-Pilot</h4>
+                        <h4>TXT2KEY Assistant</h4>
                         <div>
-                            chat gpt answer for ${data.toString()}
+                             rake:
+                            ${rake.map(d => d.keyword).join(", ")}
+                        </div>
+                        <div>
+                             gpt:
+                            ${gpt.map(d => d.keyword).join(", ")}
                         </div>
                     </div>
                 `
@@ -29,38 +95,70 @@ function run(){
             document.body.appendChild(txt2keyNode)
         }
     }
+    async function onText2KeyRequest(config, data){
+        /* Query the api */
+        let responses = await Promise.all([
+            api.queryChatGPT(data.form)
+                .then(async (response) => {
+                    return {data: await response.json(), type: "gpt"}
+                }).catch(e => {
+                console.error("queryChatGPT -could not fetch the keys", e);
+                return e;
+            }),
+            api.queryRAKE(data.form)
+                .then(async (response) => {
+                    return {data: await response.json(), type: "rake"}
+                }).catch(e => {
+                console.error("queryRAKE - could not fetch the keys", e);
+                return e;
+            })
+        ]).then(values => values)
+        console.log("txt2key request - res", {
+            responses
+        })
+        /* Dispatch the event */
+        dispatchTxt2KeyEvent(config, responses)
+    }
     window.onload = function (){
         /* Get the config from the global window object */
         const config = window["txt2key"];
         console.log("txt2key plugin has been initialized with the following config:", {
             config
         })
-        window.addEventListener("txt2key", function (event){
-            if(event.detail.type === "request"){
-                console.log("txt2key request", {
-                    event
-                })
-                let text = event.detail.data.text;
-                /* Dispatch the event */
-                dispatchTxt2KeyEvent(config, text)
-            }
-        })
 
-        const source = document.body;
-        source.addEventListener("copy", (event) => {
-            const selection = document.getSelection();
-            event.clipboardData.setData("text/plain", selection.toString());
-            event.preventDefault();
-
-            /* Perform the request to our API */
-            const output = undefined;
-            console.log("copied following value:", {
-                input: selection.toString(),
-                output
+        /* Request mode (button press) */
+        if(config.modes.includes("request")){
+            window.addEventListener("txt2key", async function (event){
+                if(event.detail.type === "request"){
+                    console.log("txt2key request", {
+                        event
+                    })
+                    let data = event.detail.data;
+                    await onText2KeyRequest(config, data)
+                }
             })
-            /* Dispatch the event */
-            dispatchTxt2KeyEvent(config, selection)
-        });
+        }
+
+        /* Clipboard mode (selection) */
+        if(config.modes.includes("clipboard")){
+            const source = document.body;
+            source.addEventListener("copy", async function (event){
+                const selection = document.getSelection();
+                event.clipboardData.setData("text/plain", selection.toString());
+                event.preventDefault();
+
+                const output = undefined;
+                console.log("copied following value:", {
+                    input: selection.toString(),
+                    output
+                })
+                let text = selection.toString();
+                let data = {
+                    title: text
+                };
+                await onText2KeyRequest(config, data)
+            });
+        }
     }
 }
 run();
--- gui-react/src/components/text2key/TTK.js
+++ gui-react/src/components/text2key/TTK.js
@@ -9,18 +9,41 @@ import Autocomplete from '@mui/material/Autocomplete';
 import { css } from '@emotion/react';
 import Slider from '@mui/material/Slider';
 import Box from '@mui/material/Box';
-import {Button} from "@mui/material";
+import {Button, Divider, Typography} from "@mui/material";
+import LoadingButton from '@mui/lab/LoadingButton';
+import CircularProgress from '@mui/material/CircularProgress';
+import LinearProgress from '@mui/material/LinearProgress';
 
 function TTK() {
-    const [keywords, setKeywords] = useState([]);
-    const [keysValue, setKeysValue] = useState("");
+    const [rake, setRake] = useState(null);
+    const [gpt, setGPT] = useState(null);
+    const [isLoading, setIsLoading] = useState(false)
+    const [form, setForm] = useState({
+        title: "Aare-Boote",
+        description: "How many boats are on the Aare river.",
+        topics: "Nature, Boats, Water",
+        publisher: "Mario Rossi"
+    });
 
     useEffect(() => {
         function onTxt2KeyEvent(){
             window.addEventListener("txt2key", function (event) {
-                console.log("got txt2key event", event);
-                const newKeywords = event.detail.data.toString().split(" ").map(k => k.trim());
-                setKeywords(newKeywords)
+                if(event.detail.type === "response"){
+                    console.log("got txt2key response event", event);
+                    setIsLoading(false);
+                    try {
+                        let gpt = event.detail.data.filter(d => d.type === "gpt")[0].data.keywords;
+                        setGPT(gpt)
+                    } catch (e){
+                        console.error("error", e);
+                    }
+                    try {
+                        let rake = event.detail.data.filter(d => d.type === "rake")[0].data;
+                        setRake(rake)
+                    } catch (e){
+                        console.error("error", e);
+                    }
+                }
             });
         }
         const init = async () => {
@@ -32,87 +55,171 @@ function TTK() {
         }
     }, [])
     return (
-        <div className="TTK"
-             css={css`
+        <div
+            className="TTK"
+            css={css`
               display: flex;
               flex-direction: column;
               align-items: center;
               justify-content: center;
             `}
         >
-            <div style={{minWidth: "50vw", maxWidth: "800px"}}>
-                <TextField
-                    id="outlined-basic"
-                    label="Title"
-                    variant="outlined"
-                    fullWidth
-                    defaultValue={"Lorem ipsum dolor sit amen"}
-                />
-                <Box mt={2}/>
-                <TextField
-                    id="outlined-basic"
-                    label="Description"
-                    variant="outlined"
-                    fullWidth
-                    multiline
-                    rows={4}
-                    defaultValue={"Lorem ipsum dolor sit amen"}
-                />
-                <Box mt={2}/>
-                <TextField
-                    id="outlined-basic"
-                    label="Topics"
-                    variant="outlined"
-                    fullWidth
-                    multiline
-                    rows={4}
-                    defaultValue={"Lorem ipsum dolor sit amen"}
-                />
-                <Box mt={2}/>
-                <TextField
-                    id="outlined-basic"
-                    label="Publisher"
-                    variant="outlined"
-                    fullWidth
-                    multiline
-                    rows={2}
-                    defaultValue={"Lorem ipsum dolor sit amen"}
-                />
-                <Box mt={2}/>
-                <Button fullWidth onClick={()=>{
-                    /* Broadcast the event to the host so that the UI can be updated */
-                    window.dispatchEvent(
-                        new CustomEvent("txt2key", { detail: {
-                                type: "request",
-                                data: {
-                                    info: "Get the keys for a given text",
-                                    text: "My text"
-                                }
-                            }})
-                    );
-                }}>Extract Keys</Button>
-                <Box mt={2}/>
-                <Autocomplete
-                    id="combo-box-demo"
-                    label="Intellikeys"
-                    options={(keywords || []).map((keyword, index) => {
-                        return {
-                            key: index,
-                           label: keyword,
-                           keyword
-                        }
-                    })}
-                    sx={{ width: 300 }}
-                    disablePortal={true}
-                    value={keysValue}
-                    onChange={(event, value, reason, details)=>{
-                        setKeysValue(value)
-                    }}
-                    renderInput={(params) => <TextField {...params} label="Intellikeys" />}
-                />
+            <div>
+                <Typography variant={"h3"} style={{marginBottom: "20px"}}>Txt2Key Demo</Typography>
+            </div>
+            <div>
+                <div
+                    css={css`
+                  display: flex;
+                `}
+                >
+                    <div
+                        css={css`
+                  display: flex;
+                  flex-direction: column;
+                  height: 100%;
+                `}
+                    >
+                        <Typography variant={"h5"} style={{marginBottom: 15}}>Input</Typography>
+                        <Box mt={5}/>
+                        <div style={{minWidth: "40vw", maxWidth: "800px"}}>
+                            <TextField
+                                id="outlined-basic"
+                                label="Title"
+                                variant="outlined"
+                                fullWidth
+                                value={form.title}
+                                onChange={(event)=>{
+                                    setForm({
+                                        ...form,
+                                        title: event.target.value
+                                    })
+                                }}
+                            />
+                            <Box mt={2}/>
+                            <TextField
+                                id="outlined-basic"
+                                label="Description"
+                                variant="outlined"
+                                fullWidth
+                                multiline
+                                rows={4}
+                                value={form.description}
+                                onChange={(event)=>{
+                                    setForm({
+                                        ...form,
+                                        description: event.target.value
+                                    })
+                                }}
+                            />
+                            <Box mt={2}/>
+                            <TextField
+                                id="outlined-basic"
+                                label="Topics"
+                                variant="outlined"
+                                fullWidth
+                                multiline
+                                rows={4}
+                                value={form.topics}
+                                onChange={(event)=>{
+                                    setForm({
+                                        ...form,
+                                        topics: event.target.value
+                                    })
+                                }}
+                            />
+                            <Box mt={2}/>
+                            <TextField
+                                id="outlined-basic"
+                                label="Publisher"
+                                variant="outlined"
+                                fullWidth
+                                multiline
+                                rows={2}
+                                value={form.publisher}
+                                onChange={(event)=>{
+                                    setForm({
+                                        ...form,
+                                        publisher: event.target.value
+                                    })
+                                }}
+                            />
+                            <Box mt={2}/>
+                            <LoadingButton  loading={isLoading} fullWidth onClick={()=>{
+                                /* Broadcast the event to the host so that the UI can be updated */
+                                setIsLoading(true)
+                                window.dispatchEvent(
+                                    new CustomEvent("txt2key", { detail: {
+                                            type: "request",
+                                            data: {
+                                                form: {
+                                                    ...form,
+                                                    topics: form.topics.split(",").map(t => t.trim())
+                                                }
+                                            }
+                                        }})
+                                );
+                            }}>Extract Keys</LoadingButton >
+                        </div>
+                    </div>
+                    <Box ml={2} mr={2}/>
+                    <div
+                        css={css`
+                  display: flex;
+                  flex-direction: column;
+                  height: 100%;
+                `}
+                    >
+                        <Typography variant={"h5"} style={{marginBottom: 15}}>Output</Typography>
+                        <Box mt={5}/>
+                        <div style={{minWidth: "40vw", maxWidth: "800px"}}>
+                            <Typography variant={"h6"}>
+                                RAKE
+                            </Typography>
+                            {
+                                isLoading ? <LinearProgress variant={"indeterminate"} /> : (
+                                    <Typography variant={"body1"}>
+                                        {
+                                            (()=>{
+                                                try {
+                                                    if(rake){
+                                                        return rake?.map(d => d.keyword).join(", ")
+                                                    }
+                                                } catch (e){
+                                                    return e.message
+                                                }
+                                            })()
+                                        }
+                                    </Typography>
+                                )
+                            }
+                            <Divider style={{margin: "15px"}}></Divider>
+                            <Typography variant={"h6"}>
+                                GPT
+                            </Typography>
+                            {
+                                isLoading ? <LinearProgress variant={"indeterminate"} />  : (
+                                    <Typography variant={"body1"}>
+                                        {
+                                            (()=>{
+                                                try {
+                                                    if(gpt){
+                                                        return gpt.map(d => d.keyword).join(", ")
+                                                    }
+                                                } catch (e){
+                                                    return e.message
+                                                }
+                                            })()
+                                        }
+                                    </Typography>
+                                )
+                            }
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    );
+    )
 }
 
 export default TTK;
